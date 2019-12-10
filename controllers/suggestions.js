f9a33d8c6687bf5b9cc03da4f65715d4c626d47f
Joe Muzina <joe.muzina@gmail.com>
2019-12-09 22:58:21 -0500

    Added page title to suggestions

--- controllers/suggestions.js
+++ controllers/suggestions.js
@@ -4,7 +4,7 @@ var api_connection = spotify_handler.spotify_connection;
 exports.get_suggestions = function(req, res, next) {
   api_connection.getPlaylist(req.session.selected_playlist).then(
     function(data) {
-      res.render('suggestions', {user: req.session.json, selected_playlist: req.session.selected_playlist, playlist_name: data.body['name'], playlist_images: data.body['images']});
+      res.render('suggestions', {title: 'Spotify Playlist Optimizer', user: req.session.json, selected_playlist: req.session.selected_playlist, playlist_name: data.body['name'], playlist_images: data.body['images']});
     },
     function(err) {
       console.log(err);
