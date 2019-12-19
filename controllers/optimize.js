7cb579f4643998192c475fb3066b60229e44ec74
Joe Muzina <joe.muzina@gmail.com>
2019-12-18 21:42:53 -0500

    Setup API traversal for playlist songs

--- controllers/optimize.js
+++ controllers/optimize.js
@@ -4,6 +4,9 @@ var api_connection = spotify_handler.spotify_connection;
 exports.get_optimize = function(req, res, next) {
   api_connection.getPlaylist(req.session.selected_playlist).then(
     function(data) {
+     for (track in data.body['tracks']['items']) {
+       console.log(data.body['tracks']['items'][track]['track']['name']);
+     }
       res.render('optimize', {title: 'Options', user: req.session.json, selected_playlist: req.session.selected_playlist, playlist_name: data.body['name'], playlist_images: data.body['images']});
     },
     function(err) {
