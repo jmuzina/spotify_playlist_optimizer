059045d600f5977213b0cd7cb20a4e34f09717b9
Joe Muzina <joe.muzina@gmail.com>
2020-01-29 19:58:55 -0500

    Now color-filtering suggestions on optimization page

--- controllers/optimize.js
+++ controllers/optimize.js
@@ -10,7 +10,11 @@ exports.get_optimize = function(req, res, next) {
       for (track in data.body['tracks']['items']) {
         tracks.push(new CLASSES.track_info(data.body['tracks']['items'][track]['track']['id'], data.body['tracks']['items'][track]['track']['name'], FUNCTIONS.artist_string(data.body['tracks']['items'][track]['track']['artists']), data.body['tracks']['items'][track]['track']['uri'], data.body['tracks']['items'][track]['track']['preview_url'], data.body['tracks']['items'][track]['track']['album']['images'][0]['url']));
       }
-       res.render('optimize', {title: 'Optimize ' + data.body['name'], user: req.session.json, playlist_tracks: JSON.parse(JSON.stringify(tracks)), playlist_name: data.body['name'], playlist_images: data.body['images'], playlist_uri: data.body['uri'].substring(17)});
+      req.session.selected_json = JSON.parse(JSON.stringify(tracks));
+
+      compared = JSON.parse(JSON.stringify(FUNCTIONS.playlist_compare(req.session.suggestions_json, req.session.selected_json)));
+
+      res.render('optimize', { title: 'Optimize ' + data.body['name'], user: req.session.json, playlist_tracks: JSON.parse(JSON.stringify(tracks)), playlist_name: data.body['name'], playlist_images: data.body['images'], playlist_uri: data.body['uri'].substring(17), suggestions: req.session.suggestions_json, comparison: compared});
      },
     function(err) {
       console.log("err: " + err);
--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 var spotify_handler = require('./spotify_auth_handler.js');
 var api_connection = spotify_handler.spotify_connection;
+const CLASSES = require('./classes.js');
 
 exports.create_playlist = function(req, res, name, private) {
     api_connection.createPlaylist(req.session.user, name, {'public': !private}).then(
@@ -11,7 +12,7 @@ exports.create_playlist = function(req, res, name, private) {
             api_connection.addTracksToPlaylist(data.body['id'], songs_to_add).then(
                 function(track_data) {
                     console.log("Playlist created and songs inserted successfully!");
-                    res.render('home', { title: 'Spotify Playlist Optimizer', user: req.session.json});
+                    res.render('home', { title: 'Spotify Playlist Optimizer', user: req.session.json, creation_success: true});
                 },
                 function(track_err) {
                     console.log(track_err);
@@ -30,24 +31,66 @@ exports.artist_string = function(arr) {
     for (artist in arr) {
         // If there's more than 1 artist, handle placement of commas and "and's"
         if (Object.keys(arr).length > 1) {
-        // "and" before last artist name
-        if (num_added == Object.keys(arr).length - 1) {
-            artists += " and " + arr[artist]['name'];
+            // "and" before last artist name
+            if (num_added == Object.keys(arr).length - 1) {
+                artists += " and " + arr[artist]['name'];
+            }
+            // Comma after all artists except the second to last one
+            else if (!(num_added == Object.keys(arr).length - 2)) {
+                artists += arr[artist]['name'] + ", ";
+            }
+            // Do nothing special for second to last artist.
+            // the last artist will have an "and" placed before their name.
+            else {
+                artists += arr[artist]['name'];
+            }
         }
-        // Comma after all artists except the second to last one
-        else if (!(num_added == Object.keys(arr).length - 2)) {
-            artists += arr[artist]['name'] + ", ";
-        }
-        // Do nothing special for second to last artist.
-        // the last artist will have an "and" placed before their name.
         else {
             artists += arr[artist]['name'];
-        }
-        }
-        else {
-        artists += arr[artist]['name'];
         }
         num_added += 1;
     }
     return artists;
+}
+
+exports.tracks_equal = function(t1, t2) {
+    return (t1.id === t2.id);
+}
+
+exports.playlist_compare = function(suggested, selected) {
+    // Should add
+    suggested_not_selected = (suggested.filter(function (n) {
+        for (var i = 0; i < selected.length; i++) {
+            if (n.id == selected[i].id) {
+                return false;
+            }
+        }
+        return true;
+    })).map(arr => arr.id);
+
+    // Should remove
+    selected_not_suggested = (selected.filter(function (n) {
+        for (var i = 0; i < suggested.length; i++) {
+            if (n.id == suggested[i].id) {
+                return false;
+            }
+        }
+        return true;
+    })).map(arr => arr.id);
+    
+    // Should keep
+    selected_suggested = (selected.filter(function (n) {
+        for (var i = 0; i < suggested.length; i++) {
+            if (n.id == suggested[i].id) {
+                return true;
+            }
+        }
+        return false;
+    })).map(arr => arr.id);
+
+    return {
+        "add": suggested_not_selected, 
+        "remove": selected_not_suggested, 
+        "keep": selected_suggested
+    };
 }
