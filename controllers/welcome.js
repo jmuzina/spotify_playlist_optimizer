cfdffc7129b58df9f28de4a4356468a526abfaea
Joe Muzina <joe.muzina@gmail.com>
2020-03-28 17:45:25 -0500

    implemented rudimentary visit tracker

--- controllers/welcome.js
+++ controllers/welcome.js
@@ -2,14 +2,27 @@ const FUNCTIONS = require('../functions.js');
 const SQL = require('../mysql_cfg.js');
 
 exports.get_welcome = function(req, res, next) {
-  console.log("[CONNECTION] " + req.connection.remoteAddress.substring(7));
-  if (!FUNCTIONS.logged_in(req.session)) {
-    res.render('welcome', { title: 'Spotify Playlist Optimizer' });
-  }
-  else {
-    FUNCTIONS.default_session(req.session);
-    res.render('welcome', { title: 'Spotify Playlist Optimizer', user: req.session.json});
-  }
+  var query = "UPDATE playlist_optimizer.stats SET connections=connections+1"
+  SQL.con.query(query, function(err, result) {
+    if (err) throw err;
+    else if (result) {
+      console.log("[CONNECTION] " + req.connection.remoteAddress.substring(7));
+    }
+  })
+
+  query = "SELECT connections FROM playlist_optimizer.stats";
+  SQL.con.query(query, function(err, result) {
+      if (err) throw err;
+      else {
+        if (!FUNCTIONS.logged_in(req.session)) {
+          res.render('welcome', { title: 'Spotify Playlist Optimizer', visits: result['0']['connections']});
+        }
+        else {
+          FUNCTIONS.default_session(req.session);
+          res.render('welcome', { title: 'Spotify Playlist Optimizer', visits: result['0']['connections'], user: req.session.json});
+        }
+      } 
+  })
 }
 
 exports.post_welcome = function(req, res, next) {
