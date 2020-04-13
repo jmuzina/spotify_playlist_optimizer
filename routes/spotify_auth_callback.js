8000a255365c78643e0b266ba9a0422955985948
Joe Muzina <joe.muzina@gmail.com>
2020-04-13 15:19:56 -0500

    Removed some old files

--- routes/spotify_auth_callback.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-let spotify_auth_callback_controller = require('../controllers/spotify_auth_callback.js');
-let welcome_controller = require('../controllers/welcome.js');
-
-router.get('/', spotify_auth_callback_controller.get_auth_callback);
-
-module.exports = router;
