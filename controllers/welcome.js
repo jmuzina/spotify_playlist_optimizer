84552c94175f9452f8785514480b2217fc91ad82
Joe Muzina <joe.muzina@gmail.com>
2020-04-24 09:22:19 -0500

    Not sure why this won't work

--- controllers/welcome.js
+++ controllers/welcome.js
@@ -2,7 +2,7 @@ const FUNCTIONS = require('../functions.js');
 const APP = require('../app.js');
 
 exports.get_welcome = function(req, res, next) {
-  res.render('welcome', { title: 'Spotify Playlist Optimizer', user: req.user, version: APP.APP_VERSION});   
+  res.render('welcome', { title: 'Spotify Playlist Optimizer', user: req.user, version: "Playlist Optimizer"});   
 }
 
 exports.post_welcome = function(req, res, next) {
